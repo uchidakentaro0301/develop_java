5086831c5d97a5e63564ecf64b81b1c06a8214ed
ken <ken@localhost>
2024-07-15 08:01:33 +0900

    first commit

---
+++ java_ec_program/src/main/java/jp/co/sss/java_ec_program/JavaEcProgramApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sss.java_ec_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEcProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEcProgramApplication.class, args);
+	}
+
+}
